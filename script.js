8077cac003c1cbc6a57ec89b3a1591331a44e777
Yujinndev <xyujinwho@gmail.com>
2023-02-02 21:51:42 +0800

    Add faqs dropdown

---
+++ script.js
@@ -0,0 +1,14 @@
+var acc = document.getElementsByClassName("dropdown");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
